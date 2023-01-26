4a0a5297fb9223d6cdb3fa8d63670ffdc524a401
Frontend JavaScript <frontendjavascript@C3-12.local>
2023-01-26 20:57:06 +0200

    recapitulare

---
+++ recapitulare/magazinstart/store.js
@@ -0,0 +1,45 @@
+const setStorageItem = (name,item) => {
+    localStorage.setItem(name, JSON.stringify(item))
+}
+const getStorageItem = (item) => {
+    let storageItem = localStorage.getItem(item)
+    if (storageItem){
+        storageItem = JSON.parse(localStorage.getItem(item))
+    } else{
+        storageItem = []
+    }
+    return storageItem
+}
+
+let store = getStorageItem('store');
+const setupStore = (products) => {
+    store = products.map((product) => {
+        const{
+            id,
+            fields: { featured, name, price, company, colors, image: img},
+        } = product;
+        const image = img[0].thumbanils.large.url;
+        return {id, featured, name, price, company, colors, image};
+    });
+    setStorageItem ('store', store);
+};
+
+const findProduct = (id) => {
+    let product = store.find((product)=> product.id === id);
+    return product;
+
+};
+
+const formatPrice = (price) => {
+    let formattedPrice = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'EUR',
+    }).format((price/100).toFixed(2))
+    return formattedPrice
+}
+
+const getElement = (selection) => {
+    const getElement = document.querySelector(selection)
+    if (element) return element
+    throw new Error(`Please check "${selection}" selector, no such element exist`)
+}
